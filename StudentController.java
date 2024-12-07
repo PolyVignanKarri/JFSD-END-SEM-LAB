6d2804fd209b1bd09c0311fa0ff74828afbebea4
PolyVignanKarri <123462549+PolyVignanKarri@users.noreply.github.com>
2024-12-07 13:24:39 +0530

    Add files via upload

---
+++ StudentController.java
@@ -0,0 +1,30 @@
+package klu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import klu.model.StudentManager;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3001/")
+@RequestMapping("/student")
+public class StudentController {
+	@Autowired
+	StudentManager SM;
+	
+	@GetMapping("/getstudents/{cpage}/{psize}")
+	public String getStudents(@PathVariable("cpage") Long cPage, @PathVariable("psize") Long pSize)
+	{
+		return SM.getData(cPage - 1, pSize);
+	}
+	
+	@GetMapping("/getpaging/{psize}")
+	public String getPaging(@PathVariable("psize") Long pSize)
+	{
+		return SM.getPaging(pSize).toString();
+	}
+}
